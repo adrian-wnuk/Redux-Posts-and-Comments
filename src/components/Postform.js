e775242d233cc17daef21c032bd8715bb3875ef4
Adrian Wnuk <adrianwnuk@MacBook-Pro-2.local>
2019-07-21 18:25:28 +0200

    posts and add post functionality working

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createPost } from "../actions/postAction";
 
 class Postform extends Component {
   constructor(props) {
@@ -24,15 +27,7 @@ class Postform extends Component {
       body: this.state.body
     };
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(post)
-    })
-      .then(response => response.json())
-      .then(data => console.log(data));
+    this.props.createPost(post);
   }
 
   render() {
@@ -71,4 +66,11 @@ class Postform extends Component {
   }
 }
 
-export default Postform;
+Postform.propTypes = {
+  createPost: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createPost }
+)(Postform);
